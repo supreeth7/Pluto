06b8ea22ec03bd1fab24e453764cd4d7364156d7
Supreeth-Basabattini <supreeth.basabattini@gmail.com>
2021-02-03 13:04:10 +0530

    added .gitignore

--- v1/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$_ENV['HOST'] = 'b3cl35heb1wnthll9jl1-mysql.services.clever-cloud.com';
-$_ENV['DB_NAME']='b3cl35heb1wnthll9jl1';
-$_ENV['USER']='u023lqhry3d7kkhw';
-$_ENV['PORT']=3306;
-$_ENV['PASSWORD']='lnYGthTbqbEygxZKtmnN';
-$_ENV['URI']='mysql://u023lqhry3d7kkhw:lnYGthTbqbEygxZKtmnN@b3cl35heb1wnthll9jl1-mysql.services.clever-cloud.com:3306/b3cl35heb1wnthll9jl1';
